f1ccf4952424bc0eaadb75d05e99431199fc5e85
faiz-webdev <faijuddin@tothenew.com>
2023-07-12 12:44:10 +0530

    5. add transaction form created

--- client/src/components/TransactionForm.js
+++ client/src/components/TransactionForm.js
@@ -1,21 +1,50 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DesktopDatePicker } from "@mui/x-date-pickers";
+import dayjs from "dayjs";
 
 export default function TransactionForm() {
+  const handleChange = () => {};
+
   return (
     <Card sx={{ minWidth: 275, marginTop: 10 }}>
       <CardContent>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
+        <Typography variant="h6">Add New Transaction</Typography>
+        <form action="">
+          <TextField
+            sx={{ marginRight: 5 }}
+            id="outlined-basic"
+            label="Amount"
+            variant="outlined"
+            size="small"
+          />
+          <TextField
+            sx={{ marginRight: 5 }}
+            id="outlined-basic"
+            label="Description"
+            variant="outlined"
+            size="small"
+          />
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DesktopDatePicker
+              label="Transaction Date"
+              inputFormat="MM/DD/YYYY"
+              onChange={handleChange}
+              renderInput={(params) => <TextField sx={{ marginRight: 5 }} size="small" {...params} />}
+            />
+          </LocalizationProvider>
+          <Button type="submit" variant="contained">
+            Submit
+          </Button>
+        </form>
       </CardContent>
     </Card>
   );
